6030fe0de7fda6b61b165facdb80c6a73c1bf8dc
marcelklehr <mklehr@gmx.net>
2023-02-03 12:17:25 +0000

    Deploying to gh-pages from @ magpie-ea/magpie3-dpp-mt@747eb412f412b46e32589ec21d0b05afc73571cb 🚀

--- src/magpie.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  experimentId: '106',
-  serverUrl: 'https://mcmpact.ikw.uni-osnabrueck.de/magpie/',
-  socketUrl: 'wss://mcmpact.ikw.uni-osnabrueck.de/magpie/socket',
-  completionUrl: 'https://app.prolific.co/submissions/complete?cc=CNXFIYQD',
-  contactEmail: 'vini.macuch@gmail.com',
-  mode: 'prolific'
-};
